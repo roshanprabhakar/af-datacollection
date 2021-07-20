e0fb8e03e84e7a41c19ccea45a2e06e771e80d54
kaiserowilliams@gmail.com <kaiserowilliams@gmail.com>
2021-07-20 16:22:35 -0700

    created Array buffers

--- observeroutputs/observer.js
+++ observeroutputs/observer.js
@@ -1,6 +1,7 @@
 var outputdata = []; //observed outputs and time stap array format
 var list = []; //list of actiondata; 2D array format
 var timestart;
+var JSONlength = 0;
 
 //time when page loads
 window.onload = () => {
@@ -10,7 +11,6 @@ timestart = Date.now();
 var outputdataJSON = {}; //observed outputs and time stap JSON format
 var listJSON = []; //list of actiondata of JSON
 
-var random; 
 //add actiondata to list
 function action(clicked_id) {
     outputdataJSON = {action : clicked_id, timestamp : parseInt(Date.now() - timestart)};
@@ -20,16 +20,19 @@ function action(clicked_id) {
     list.push(outputdata);
 
     document.getElementById("actions").innerHTML = list;
+
+    JSONlength += 1;
 }
 
 
 // when "finish" button is pressed, a text file containing JSON list is downloaded
+
+/*
 function save() { 
-    random = JSON.stringify(listJSON);
+    var random = JSON.stringify(listJSON);
     var blob = new Blob([random], {type: "text/plain;charset=utf-8"});
     saveAs(blob, "outputs.txt");
 }
+*/
 
 
-
-// Date.now() method returns the number of milliseconds elapsed since January 1, 1970 00:00:00 UTC
--- observeroutputs/outputbytes.js
+++ observeroutputs/outputbytes.js
@@ -1,18 +1,48 @@
+var timebuffer;
+var dv1;
+var actionbuffer;
+var dv2;
+var buf;
+var bufview;
+var arr = [];
 
 function JSONtobit() {
-    var timebuffer = new ArrayBuffer(listJSON.length);
-    let dv1 = new DataView(timebuffer);
+    timebuffer = new ArrayBuffer(JSONlength * 4); //Array buffer for timestamps
+    dv1 = new Int32Array(timebuffer); //dataview for timebuffer
 
-    var actionbuffer = new ArrayBuffer(listJSON.length);
-    let dv2 = new DataView(actionbuffer);
-
-
-    for (let i = 0; i < listJSON.length; i++) {
-        dv1.setInt32(i, listJSON[i].timestamp);
-        dv2.setBigInt64(i, listJSON[i].action);
+    actionbuffer = new ArrayBuffer(JSONlength * 8); //Array buffer for actions
+    dv2 = new BigInt64Array(actionbuffer); //dataview from actionbuffer
+    
+    for (let i = 0; i < dv1.length; i++) {
+        dv1[i] = listJSON[i].timestamp;
+        // dv2[i] = listJSON[i].action;
+        arr[i] = stringbuff(listJSON[i].action);
     }
+}
 
+function stringbuff(str) {
+
+    buf = new ArrayBuffer(str.length * 2);
+    bufView = new Uint16Array(buf);
+    for(let i = 0, strLen = str.length; i< strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+    
 }
+
+
+
+
+
+/*
 function bittoJSON() {
-    //Do I make another JSON and combine the timestamp and action?
+    var outputs = [];
+    var outJSON = {};
+    for(let i = 0; i < (timebuffer.byteLength / 4); i++) {
+        outJSON = {timestamp : dv1[i]};
+        outputs.push(outJSON);
+    }
+    return outputs;
 }
+*/
